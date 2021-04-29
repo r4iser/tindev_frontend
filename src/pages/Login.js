0067428e268ccc9296532bbb70a0d00ea38ba504
r4iser <71898254+r4iser@users.noreply.github.com>
2021-04-29 13:19:37 -0300

    Set up Main(logged) page, consulting the backend + Styles
    +2 svg assets
    +axios setup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import logo from '../assets/logo.svg';
 import './Login.css';
+import api from '../services/api';
 
 export default function Login({ history }) {
     const [username, setUsername] = useState('');
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
-        console.log(username);
 
-        history.push('/main');
+        const response = await api.post('/devs', {
+            username: username,
+        })
+
+        console.log(response);
+
+        const { _id } = response.data;
+
+        history.push(`/dev/${_id}`);
     }
 
     return (
